e1949fafead0ad4a82face0321884c09af8ab42a
shjeon0304 <shjeon0304@snu.ac.kr>
2024-02-03 19:52:45 +0900

    템플릿상속 url 관리

--- agmo_project/urls.py
+++ agmo_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import homeapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homeapp.views.home, name='home'),
     path('new/', homeapp.views.new, name = 'new'),
+    path('homeapp/', include('homeapp.urls'))
 ]
--- homeapp/views.py
+++ homeapp/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects.order_by('-id')
+    return render(request, 'home.html',{'blogs':blogs})
 
 def new(request):
-    return render(request, 'new.html')
+    full_text = request.GET['fulltext']
+
+    word_list = full_text.split()
+
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+
+    return render(request, 'new.html', {'fulltext':full_text,'wordlist':len(word_list),'dictionary': word_dictionary.items()})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html',{'blog':blog_detail})
