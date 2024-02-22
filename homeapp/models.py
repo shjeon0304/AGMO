23ab7b43aa6fa8cdf8f602fb62d1aee08a372725
shjeon0304 <shjeon0304@snu.ac.kr>
2024-02-22 19:26:31 +0900

    Add files via upload

--- homeapp/models.py
+++ homeapp/models.py
@@ -40,5 +40,58 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cropdata(models.Model):
+    CROP_CHOICES = {
+        ('팥','팥'),
+        ('밀','밀')
+    }
+    CROPWORK_CHOICES = {
+        ('파종','파종'),
+        ('제초','제초'),
+        ('배수구 정비','배수구 정비'),
+        ('토입','토입'),
+        ('답압','답압'),
+        ('추비','추비'),
+        ('방제','방제'),
+        ('수확및 탈곡','수확및 탈곡'),
+        ('수매','수매'),
+        ('북주기','북주기'),
+        ('김매기','김매기'),
+        ('배수작업','배수작업'),
+        ('수확','수확'),
+    }
+    PESTICIDE_CHOICES = {
+        ('알라코입제','알라코입제'),
+        ('파라코입제','파라코입제'),
+        ('부타유제','부타유제'),
+        ('수화유황','수화유황'),
+        ('두엄','두엄'),
+        ('적미방제약','적미방제약')
+    }
+    crop = models.CharField(max_length=50, choices=CROP_CHOICES)
+    fieldNum = models.IntegerField()
+    cropWork = models.CharField(max_length=50, choices=CROPWORK_CHOICES)
+    pesticide = models.CharField(max_length=50, choices=PESTICIDE_CHOICES) 
+    date = models.DateTimeField()
+    content = models.CharField(max_length=4000)
+
+    def __str__(self):
+        return self.title
+
+'''class Cropdiary(models.Model):
     
+    CROP_CHOIECES = {
+        ('red-bean','팥'),
+        ('wheat','밀')
+
+    }
+
+
+    crop = models.CharField(max_length=80, choilces=CROP_CHOICES, null=True)
+
+    def get_absolute_url(self):
+        return reverse("model_detail", kwargs={"username": self.username})
+'''
 
