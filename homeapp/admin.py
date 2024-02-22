d31af904ccdd662ac8fe8a53481a4d59521f29ff
Woojin-jiin <158709063+Woojin-jiin@users.noreply.github.com>
2024-02-23 08:55:30 +0900

    Update admin.py

--- homeapp/admin.py
+++ homeapp/admin.py
@@ -5,3 +5,14 @@ from .models import Blog
 # Register your models here.
 
 admin.site.register(Blog)
+admin.site.register(Crop)
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 1
+
+class FarmlandAdmin(admin.ModelAdmin):
+    inlines = [
+        TaskInline,
+    ]
+
+admin.site.register(Farmland, FarmlandAdmin)
