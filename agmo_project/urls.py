921baf2913338eec6ba29ad4966ad2544dd1ad3d
shjeon0304 <shjeon0304@snu.ac.kr>
2024-02-23 09:02:16 +0900

    Update urls.py

--- agmo_project/urls.py
+++ agmo_project/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import homeapp.views
-import homepage.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
